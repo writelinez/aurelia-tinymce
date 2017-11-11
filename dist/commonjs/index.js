9afaf567928048b911894fcb93365a41d913bba1
Chris Bardsley <chris.bardsley@writelinez.com>
2017-11-10 20:34:14 -0700

    removed dist folder from source control

--- dist/commonjs/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function configure(aurelia) {
-    aurelia.globalResources([]);
-}
-exports.configure = configure;
--- dist/native-modules/index.js
+++
@@ -1,3 +0,0 @@
-export function configure(aurelia) {
-    aurelia.globalResources([]);
-}
