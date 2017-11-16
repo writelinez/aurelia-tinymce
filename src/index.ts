dfffc2d1a526af3a22382cb3263264b68e5a94be
Chris Bardsley <chris.bardsley@writelinez.com>
2017-11-15 20:46:59 -0700

    fixed an issue where the theme files where not being properly compiled in to the vender bundle

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {TinyMce} from './elements/tiny-mce/tiny-mce';
+import {ThemeModern} from './core/tinymce-modules/themes/modern/theme-modern';
+import {ThemeMobile} from './core/tinymce-modules/themes/mobile/theme-mobile';
+import {ThemeInline} from './core/tinymce-modules/themes/inline/theme-inline';
 
 export function configure(aurelia: FrameworkConfiguration) {
 
     aurelia.globalResources([
         './elements/tiny-mce/tiny-mce'
     ]);
+
+    aurelia.transient(ThemeModern);
+    aurelia.transient(ThemeMobile);
+    aurelia.transient(ThemeInline);
 }
 export {TinyMce};
